047a87a7fb204fc0cf0283453abb508425650742
dani3ll0b017 <dani3ll0b017@gmail.com>
2026-04-09 20:20:59 -0600

    Add files via upload

---
+++ Proyecto-reparacion/JS/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.querySelector('#menu ul');
+
+    if(menuToggle && menu) {
+        menuToggle.addEventListener('click', () => {
+            menu.classList.toggle('show');
+        });
+    }
+
+    const contactForm = document.getElementById('contact-form');
+    
+    if (contactForm) {
+        contactForm.addEventListener('submit', function(evento) {
+            evento.preventDefault(); // Evita el envío por defecto para validar
+            
+            const emailInput = document.getElementById('email').value;
+            const messageInput = document.getElementById('mensaje').value;
+
+            
+            if (emailInput.trim() === '' || messageInput.trim() === '') {
+                alert('Por favor, completa los campos requeridos (Correo y Mensaje).');
+                return;
+            }
+
+            if (!emailInput.includes('@')) {
+                alert('Por favor ingresa un correo electrónico válido.');
+                return;
+            }
+
+            alert('¡Gracias! Tu mensaje ha sido enviado correctamente. Te contactaremos pronto.');
+            contactForm.reset();
+        });
+    }
+});
