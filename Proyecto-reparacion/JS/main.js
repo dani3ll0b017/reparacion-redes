1da3c072ca6253288fa2506a83ed75558b9bc780
CarlosAckerman13 <care_ackermane@unicah.edu>
2026-04-12 10:26:56 -0600

    Proyecto-reparacion/JS/main.js

--- Proyecto-reparacion/JS/main.js
+++ Proyecto-reparacion/JS/main.js
@@ -33,4 +33,106 @@ document.addEventListener('DOMContentLoaded', () => {
             contactForm.reset();
         });
     }
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    let miCarusel = new Carousel("caruselPrincipal");
+    miCarusel.init();
+});
+
+class Carousel {
+    constructor(carouselId, tickTimeInSeconds = 3){
+        this.carouselHolder = document.getElementById(carouselId);
+        this.track = this.carouselHolder.querySelector(".track");
+        this.slides = [...this.track.querySelectorAll(".slide")];
+        this.minLimit = 0;
+        this.maxLimit = this.slides.length - 1;
+        this.currentIndex = 0;
+        this.tickTime = tickTimeInSeconds * 1000;
+        this.tickerId = null;
+        this.direction = 1;
+    }
+    init() {
+        // Generar Botones
+        this.generateNavigationUI();
+        this.tick();
+    }
+    tick(){
+        this.tickerId = setTimeout(
+            ()=>{
+                this.moveNext();
+                this.tick();
+            },
+            this.tickTime
+        );
+    }
+    moveNext(){
+        let tmpNewIndex = this.currentIndex + this.direction;
+        if (tmpNewIndex > this.maxLimit) {
+            tmpNewIndex = this.maxLimit - 1;
+            this.direction = -1;
+        }
+        if (tmpNewIndex < this.minLimit) {
+            tmpNewIndex = this.minLimit + 1;
+            this.direction = 1;
+        }
+        this.moveTo(tmpNewIndex);
+    }
+
+    moveTo(newIndex) {
+        this.currentIndex = newIndex;
+        this.track.style.left = `${-100 * this.currentIndex}vw`;
+    }
+
+  generateNavigationUI() {
+    let btnLeft = document.createElement("BUTTON");
+    let btnRight = document.createElement("BUTTON");
+
+    btnLeft.classList.add("carousel-btn");
+    btnLeft.classList.add("btnleft");
+    btnRight.classList.add("carousel-btn");
+    btnRight.classList.add("btnright");
+
+    btnLeft.textContent = "<";
+    btnRight.textContent = ">";
+
+    btnLeft.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        clearTimeout(this.tickerId);
+        this.direction = -1;
+        this.moveNext();
+        this.tick();
+    });
+
+    btnRight.addEventListener("click", (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        clearTimeout(this.tickerId);
+        this.direction = 1;
+        this.moveNext();
+        this.tick();
+    });
+
+    this.carouselHolder.appendChild(btnLeft);
+    this.carouselHolder.appendChild(btnRight);
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
